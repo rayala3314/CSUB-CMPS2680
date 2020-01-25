f24d8342a00b3763e3f517b132561cf868de1929
rayala3314 <rayala19@csub.edu>
2020-01-25 13:51:39 -0800

    Personal Webpage

---
+++ final/js/travel.js
@@ -0,0 +1,12 @@
+var button = document.getElementById("sendMessage");
+var content = document.getElementById("content");
+var gallery = document.getElementById("")
+
+function message() {
+    document.getElementById("content").innerHTML = " njsdf";
+    alert('Thank you for your message! I will get back to you as soon as possible');
+
+
+}
+console.log(button);
+console.log(content);
