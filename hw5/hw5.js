3c5dbab323d472dfb47f4623990860136d63920c
rayala3314 <rayala19@csub.edu>
2020-01-25 13:54:22 -0800

    Simple Javascript example

---
+++ hw5/hw5.js
@@ -0,0 +1,5 @@
+alert("Raymundo Ayala");
+var num1 = prompt("Please enter a number:", 0);
+var num2 = prompt("Please enter another number:", 0);
+
+document.write(`The product of ${num1} and ${num2} is ${num1 * num2}` );
